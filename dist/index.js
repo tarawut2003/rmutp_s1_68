dbaa85e40a2148df5b589e97165c4e3177ae63c8
tarawut <tarawut-s@rmutp.ac.th>
2025-08-27 12:48:07 +0700

    create app with hono

---
+++ dist/index.js
@@ -0,0 +1,9 @@
+import { Hono } from "hono";
+const app = new Hono();
+app.get("/", (c) => c.text("Hello World"));
+app.get("/about", (c) => {
+    return c.json({
+        message: "Tarawut"
+    });
+});
+export default app;
--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,5 @@
 import { serve } from "@hono/node-server";
 import app from "./index.js";
-
-serve(app,(info)=>{
+serve(app, (info) => {
     console.log(`Server is Running on ${info.port}`);
 });
