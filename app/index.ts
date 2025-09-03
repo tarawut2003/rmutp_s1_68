246f101940fba680f124b5167687ac55ed630b0d
tarawut <tarawut-s@rmutp.ac.th>
2025-09-03 16:45:20 +0700

    update

--- app/index.ts
+++ app/index.ts
@@ -1,6 +1,7 @@
 import { Hono } from "hono";
 //import { PrismaClient } from "../generated/prisma/client";
 import { PrismaClient } from "@prisma/client";
+import * as bcrypt from "bcrypt"; 
 
 const prisma = new PrismaClient();
 const app = new Hono();
@@ -11,10 +12,23 @@ app.get("/about",(c) => {
         message:"Tarawut"
     })
 });
+
 app.get("/profile", async(c) => {
-    ///
     const profiles = await prisma.profile.findMany();
     return c.json(profiles);
+});
+
+app.post("/profile", async(c)=>{
+    const body = await c.req.json();
+    console.log('input of profile', body);
+    console.log('body.password(original)', body.password);
+
+    //encode password
+    const passwordHash = await bcrypt.hash(body.password, 18);
+    console.log('hash.password(after)', passwordHash);
+    //save to db 
+    
+    return c.json({message: "create profile"});
 });
 
 export default app;
