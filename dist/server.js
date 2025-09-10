42cb29f2dde32eb0e51f0c2358686c9ef79a31c3
tarawut2003 <tarawut-s@rmutp.ac.th>
2025-09-10 17:56:15 +0700

    Delete dist directory

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const node_server_1 = require("@hono/node-server");
-const index_js_1 = require("./index.js");
-(0, node_server_1.serve)(index_js_1.default, (info) => {
-    console.log(`Server is Running on ${info.port}`);
-});
